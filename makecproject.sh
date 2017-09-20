7cf59f49ea26472950065114dfbdf0cdf3e90bc8
Sebastian <sebastian.rivera@jcxsoftware.com>
2017-09-19 21:52:48 -0400

    new updates for git

--- makecproject.sh
+++ makecproject.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-echo "Project Name: "
-read project
+echo "What language will the project be written in?(Write C or C++)"
+read language
+	echo "Project Name: "
+	read project
 
-mkdir  $project
-mkdir  $project/src
-mkdir  $project/includes
-mkdir  $project/build
-mkdir  $project/bin
-mkdir  $project/lib
-mkdir  $project/doc
+	mkdir  $project
+	mkdir  $project/src
+	mkdir  $project/includes
+	mkdir  $project/build
+	mkdir  $project/bin
+	mkdir  $project/lib
+	mkdir  $project/doc
 
-touch $project/src/main.cpp
-
-
+$main = ""
+if [$language = "C++"];then 
+	$main = "main.cpp"
+	touch $project/src/$main
+else if [$language= "C"];then 
+	$main="main.c"
+	touch $project/src/$main
+cd $project
 ##VIM
 ##YCM Generator for Project dependent compilation flags, makes my life easier. 
-./config_gen.py $project
-vim $project/src/main.cpp
+echo "Vim or emacs?"
+read editor
+
+if [editor = "Vim"]; then
+	./config_gen.py $project
+else if [editor = "Emacs"]; then
+	bear make
+
+touch CMakeLists.txt
+vim src/$main
